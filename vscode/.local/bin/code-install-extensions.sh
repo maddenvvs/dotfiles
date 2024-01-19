958f5adcdcaae54fc641070a02835a4ca06e1d9e
maddenvvs <maddenvvs@gmail.com>
2024-01-19 19:18:22 +0000

    Migrate from VSCodium to VSCode

--- vscode/.local/bin/code-install-extensions.sh
+++ vscode/.local/bin/code-install-extensions.sh
@@ -2,10 +2,6 @@
 
 readonly VSCODE_INSTALLED_EXTENSIONS="${HOME}/.config/vscode/installed-extensions"
 
-if [[ -x "$(command -v codium)" ]]; then
-    xargs -L 1 codium --install-extension < "${VSCODE_INSTALLED_EXTENSIONS}-vscodium"
-fi
-
 if [[ -x "$(command -v code)" ]]; then
     xargs -L 1 code --install-extension < "${VSCODE_INSTALLED_EXTENSIONS}-vscode"
 fi
--- vscode/.local/bin/code-store-extensions.sh
+++ vscode/.local/bin/code-store-extensions.sh
@@ -2,10 +2,6 @@
 
 VSCODE_INSTALLED_EXTENSIONS="${HOME}/.config/vscode/installed-extensions"
 
-if [[ -x "$(command -v codium)" ]]; then
-    codium --list-extensions > "${VSCODE_INSTALLED_EXTENSIONS}-vscodium"
-fi
-
 if [[ -x "$(command -v code)" ]]; then
     code --list-extensions > "${VSCODE_INSTALLED_EXTENSIONS}-vscode"
 fi
