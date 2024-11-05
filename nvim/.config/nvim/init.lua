b2e34c9a057a9011f02462ad1493fffd1543daae
maddenvvs <maddenvvs@gmail.com>
2024-11-05 15:47:33 +0000

    Add nvim-orgmode plugin

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -4,3 +4,12 @@ vim.api.nvim_command('set runtimepath^=~/.vim runtimepath+=~/.vim/after')
 vim.api.nvim_command('let &packpath = &runtimepath')
 vim.api.nvim_command('source ~/.vim/vimrc')
 
+-- nvim-orgmode configuration.
+-- https://github.com/nvim-orgmode/orgmode/blob/master/DOCS.md
+require('orgmode').setup({
+  org_agenda_files = {
+    '~/orgs/**/*',
+  },
+  win_split_mode = 'vertical',
+})
+
