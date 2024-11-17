743ce020364486ef63d44ed4dc3a996c4e11081b
maddenvvs <maddenvvs@gmail.com>
2024-11-17 14:34:47 +0000

    Update keymap descriptions to look better in which-key

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -29,7 +29,7 @@ return {
         else
           gs.nav_hunk("next")
         end
-      end, "git next [c]hange")
+      end, "next [c]hange")
 
       map("n", "[c", function()
         if vim.wo.diff then
@@ -37,20 +37,20 @@ return {
         else
           gs.nav_hunk("prev")
         end
-      end, "git previous [c]hange")
+      end, "previous [c]hange")
 
-      map("n", "]C", function() gs.nav_hunk("last") end, "git last [C]hange")
-      map("n", "[C", function() gs.nav_hunk("first") end, "git first [C]hange")
-      map({ "n", "v" }, "<leader>hs", ":Gitsigns stage_hunk<CR>", "git [h]unk [s]tage")
-      map({ "n", "v" }, "<leader>hr", ":Gitsigns reset_hunk<CR>", "git [h]unk [r]eset")
-      map("n", "<leader>hS", gs.stage_buffer, "git [S]tage buffer")
-      map("n", "<leader>hu", gs.undo_stage_hunk, "git [h]unk [u]ndo stage")
-      map("n", "<leader>hR", gs.reset_buffer, "git [R]eset buffer")
-      map("n", "<leader>hp", gs.preview_hunk_inline, "git [h]unk [p]review")
-      map("n", "<leader>hb", function() gs.blame_line({ full = true }) end, "git [b]lame line")
-      map("n", "<leader>hB", function() gs.blame() end, "git [B]lame buffer")
-      map("n", "<leader>hd", gs.diffthis, "git [d]iff against index")
-      map("n", "<leader>hD", function() gs.diffthis("~") end, "git [D]iff against last commit")
+      map("n", "]C", function() gs.nav_hunk("last") end, "last [C]hange")
+      map("n", "[C", function() gs.nav_hunk("first") end, "first [C]hange")
+      map({ "n", "v" }, "<leader>hs", ":Gitsigns stage_hunk<CR>", "[h]unk [s]tage")
+      map({ "n", "v" }, "<leader>hr", ":Gitsigns reset_hunk<CR>", "[h]unk [r]eset")
+      map("n", "<leader>hS", gs.stage_buffer, "[S]tage buffer")
+      map("n", "<leader>hu", gs.undo_stage_hunk, "[h]unk [u]ndo stage")
+      map("n", "<leader>hR", gs.reset_buffer, "[R]eset buffer")
+      map("n", "<leader>hp", gs.preview_hunk_inline, "[h]unk [p]review")
+      map("n", "<leader>hb", function() gs.blame_line({ full = true }) end, "[b]lame line")
+      map("n", "<leader>hB", function() gs.blame() end, "[B]lame buffer")
+      map("n", "<leader>hd", gs.diffthis, "[d]iff against index")
+      map("n", "<leader>hD", function() gs.diffthis("~") end, "[D]iff against last commit")
 
       -- map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
     end,
