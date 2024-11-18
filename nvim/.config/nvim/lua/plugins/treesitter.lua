675ac4fcec68efcfaf88c4e4da26d28e4303101e
maddenvvs <maddenvvs@gmail.com>
2024-11-18 19:03:44 +0000

    Add nvim-treesitter

---
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,28 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function ()
+    local configs = require("nvim-treesitter.configs")
+
+    configs.setup({
+      ensure_installed = {
+        "lua",
+        "vimdoc",
+      },
+      sync_install = false,
+      auto_install = false,
+
+      highlight = { enable = true },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-Space>",
+          node_incremental = "<C-Space>",
+          scope_incremental = false,
+          node_decremental = "<BS>",
+        },
+      },
+      -- indent = { enable = true },
+    })
+  end,
+}
