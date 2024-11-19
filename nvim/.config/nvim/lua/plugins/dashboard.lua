ac97ac5d95aa477eb1ad1bab9581e37d88d3436f
maddenvvs <maddenvvs@gmail.com>
2024-11-19 15:06:56 +0000

    Add two more plugins for nvim

---
+++ nvim/.config/nvim/lua/plugins/dashboard.lua
@@ -0,0 +1,47 @@
+local function open_folder(path)
+  return function()
+    vim.cmd("cd " .. path)
+    vim.cmd("Neotree show focus")
+  end
+end
+
+
+return {
+  'nvimdev/dashboard-nvim',
+  dependencies = {
+    'nvim-tree/nvim-web-devicons'
+  },
+  event = 'VimEnter',
+  opts = {
+    theme = "hyper",
+    shortcut_type = "letter",
+    shuffle_letter = false,
+    hide = {
+      statusline = true,
+      tabline = true,
+      winbar = true,
+    },
+    config = {
+      disable_move = true,
+      week_header = { enable = true },
+      packages = { enable = true },
+      shortcut = {
+        {
+          desc = "Dotfiles ",
+          key = "d",
+          action = open_folder("$DOTFILES"),
+        },
+        {
+          desc = "Orgs ",
+          key = "o",
+          action = open_folder("$HOME/orgs"),
+        },
+        {
+          desc = "Lazy ",
+          key = "l",
+          action = "Lazy",
+        },
+      },
+    },
+  },
+}
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,28 +1,54 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function ()
-    local configs = require("nvim-treesitter.configs")
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function ()
+      local configs = require("nvim-treesitter.configs")
 
-    configs.setup({
-      ensure_installed = {
-        "lua",
-        "vimdoc",
-      },
-      sync_install = false,
-      auto_install = false,
-
-      highlight = { enable = true },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-Space>",
-          node_incremental = "<C-Space>",
-          scope_incremental = false,
-          node_decremental = "<BS>",
+      configs.setup({
+        ensure_installed = {
+          "lua",
+          "vimdoc",
         },
-      },
-      -- indent = { enable = true },
-    })
-  end,
+        sync_install = false,
+        auto_install = false,
+
+        highlight = { enable = true },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<C-Space>",
+            node_incremental = "<C-Space>",
+            scope_incremental = false,
+            node_decremental = "<BS>",
+          },
+        },
+        textobjects = {
+          move = {
+            enable = true,
+            goto_next_start = {
+              ["]f"] = "@function.outer",
+              ["]a"] = "@parameter.inner"
+            },
+            goto_next_end = {
+              ["]F"] = "@function.outer",
+              ["]A"] = "@parameter.inner"
+            },
+            goto_previous_start = {
+              ["[f"] = "@function.outer",
+              ["[a"] = "@parameter.inner",
+            },
+            goto_previous_end = {
+              ["[F"] = "@function.outer",
+              ["[A"] = "@parameter.inner",
+            },
+          },
+        },
+        -- indent = { enable = true },
+      })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+  },
 }
