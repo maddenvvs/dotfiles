946051c32ac101ca1ada81d8f518856b7687b397
maddenvvs <maddenvvs@gmail.com>
2024-11-19 16:51:13 +0000

    Add conform auto-formatting for nvim

---
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = {},
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = {
+      "williamboman/mason.nvim",
+    },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {},
+      })
+    end,
+  },
+}
