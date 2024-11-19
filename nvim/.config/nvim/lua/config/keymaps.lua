b4357f76c89858b03d167cd3eef4cb1c981dbac8
maddenvvs <maddenvvs@gmail.com>
2024-11-19 19:18:01 +0000

    Add keymappings for window navigation

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -4,22 +4,36 @@ vim.g.mapleader = " "
 -- Set local leader key.
 vim.g.maplocalleader = " "
 
+-- Move to window using the <Ctrl>+[hjkl] keys.
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Go to left window", silent = true, remap = true })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Go to lower window", silent = true, remap = true })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Go to upper window", silent = true, remap = true })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Go to right window", silent = true, remap = true })
+
 -- Save a buffer both in without leaving the current mode.
-vim.keymap.set({ "i", "n", "v" }, "<C-S>", "<Cmd>write<CR>", { silent = true, desc = "Save buffer" })
+vim.keymap.set({ "i", "n", "v" }, "<C-S>", "<Cmd>write<CR>", { desc = "Save buffer", silent = true })
 
 -- Maintain Visual Mode after shifting > and <.
-vim.keymap.set("x", "<", "<gv", { silent = true, desc = "Shift left (stay visual)" })
-vim.keymap.set("x", ">", ">gv", { silent = true, desc = "Shift right (stay visual)" })
+vim.keymap.set("x", "<", "<gv", { desc = "Shift left (stay visual)", silent = true })
+vim.keymap.set("x", ">", ">gv", { desc = "Shift right (stay visual)", silent = true })
 
 -- Move visual blocks down and up.
-vim.keymap.set("x", "J", ":m '>+1<CR>gv=gv", { silent = true, desc = "Move down (stay visual)" })
-vim.keymap.set("x", "K", ":m '<-2<CR>gv=gv", { silent = true, desc = "Move up (stay visual)" })
+vim.keymap.set("x", "J", ":m '>+1<CR>gv=gv", { desc = "Move down (stay visual)", silent = true })
+vim.keymap.set("x", "K", ":m '<-2<CR>gv=gv", { desc = "Move up (stay visual)", silent = true })
+
+-- Clear search, diff update and redraw.
+-- taken from runtime/lua/_editor.lua
+-- Previous version:
+-- vim.keymap.set("n", "<C-L>", ":nohlsearch<C-R>=has('diff')?'<Bar>diffupdate':''<CR><CR><C-L>", { silent = true })
+vim.keymap.set(
+  "n",
+  "<leader>ur",
+  "<Cmd>nohlsearch<Bar>diffupdate<Bar>normal! <C-L><CR>",
+  { desc = "Redraw / Clear hlsearch / Diff Update", silent = true }
+)
 
 -- Nvim already redefines them the way below. There is no need to difine them again.
 -- See ':help default-mappings'.
 
 -- Make Y behave like C and D (do operation from the cursor to the end of line).
 -- vim.keymap.set("n", "Y", "y$", { silent = true })
-
--- Clear the highlighting of ':set hlsearch'.
--- vim.keymap.set("n", "<C-L>", ":nohlsearch<C-R>=has('diff')?'<Bar>diffupdate':''<CR><CR><C-L>", { silent = true })
