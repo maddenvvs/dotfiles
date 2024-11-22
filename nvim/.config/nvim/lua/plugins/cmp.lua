4b8b5f3cd8096396812710cbaf95bc1df704086f
maddenvvs <maddenvvs@gmail.com>
2024-11-22 09:39:27 +0000

    nvim: add cmdline completion via nvim-cmp

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -45,5 +45,16 @@ return {
         { name = "buffer" },
       },
     })
+
+    -- Use cmdline & path source for ':' (if `native_menu` is enabled, this won't work anymore).
+    cmp.setup.cmdline(":", {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources({
+        { name = "path" },
+      }, {
+        { name = "cmdline" },
+      }),
+      matching = { disallow_symbol_nonprefix_matching = false },
+    })
   end,
 }
