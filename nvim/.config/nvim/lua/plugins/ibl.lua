a80251a3d46c06c9165f31af025e99aceea472cb
maddenvvs <maddenvvs@gmail.com>
2024-11-24 17:19:47 +0000

    nvim: configure indent-blankline

--- nvim/.config/nvim/lua/plugins/ibl.lua
+++ nvim/.config/nvim/lua/plugins/ibl.lua
@@ -6,6 +6,19 @@ return {
       char = "│",
       tab_char = "│",
     },
+    scope = {
+      char = "▍",
+      show_start = false,
+      show_end = false,
+      include = {
+        node_type = {
+          lua = {
+            "return_statement",
+            "table_constructor",
+          },
+        },
+      },
+    },
     exclude = {
       filetypes = {
         "dashboard",
