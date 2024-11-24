7f625a450cd5e8ad72cf0c0e8f4a568566933fc8
maddenvvs <maddenvvs@gmail.com>
2024-11-24 18:20:32 +0000

    nvim: update kanagawa theme

--- nvim/.config/nvim/lua/plugins/kanagawa.lua
+++ nvim/.config/nvim/lua/plugins/kanagawa.lua
@@ -3,6 +3,30 @@ return {
   lazy = false,
   priotiry = 1000,
   config = function()
-    require("kanagawa").load("wave")
+    local kg = require("kanagawa")
+
+    kg.setup({
+      colors = {
+        theme = {
+          all = {
+            ui = {
+              -- Remove gutter background.
+              -- https://github.com/rebelot/kanagawa.nvim?tab=readme-ov-file#remove-gutter-background
+              bg_gutter = "none",
+            },
+          },
+        },
+      },
+      overrides = function(colors)
+        return {
+          IblScope = {
+            -- Slightly brighter scope bars in `ibl` plugin.
+            fg = colors.theme.ui.special,
+          },
+        }
+      end,
+    })
+
+    kg.load("wave")
   end,
 }
