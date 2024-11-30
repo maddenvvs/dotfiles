e813dddf5e2f00999fe4a0da3a17c36645782521
maddenvvs <maddenvvs@gmail.com>
2024-11-30 18:46:53 +0000

    nvim: add snacks module

---
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,46 @@
+return {
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  keys = {
+    {
+      "<Leader>bd",
+      function()
+        Snacks.bufdelete.delete()
+      end,
+      desc = "[d]elete buffer",
+      silent = true,
+    },
+  },
+  opts = {
+    bufdelete = { enabled = true },
+    notifier = { enabled = true },
+    dashboard = {
+      preset = {
+        keys = {
+          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+          {
+            icon = " ",
+            key = "c",
+            desc = "Config",
+            action = ":lua Snacks.dashboard.pick('files', {cwd = '$DOTFILES'})",
+          },
+          {
+            icon = " ",
+            key = "o",
+            desc = "Orgfiles",
+            action = ":lua Snacks.dashboard.pick('files', {cwd = '$HOME/orgs'})",
+          },
+          { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+        },
+      },
+      formats = {
+        key = function(item)
+          return { { "[", hl = "key" }, { item.key, hl = "key" }, { "]", hl = "key" } }
+        end,
+      },
+    },
+  },
+}
